cab89d3c7f0aed5c0b3c5cefa15fece4a478ac1b
LAPTOP-QCIUTNP8\a0919 <a0919025694@gmail.com>
2023-12-05 21:31:27 +0800

    map10

--- src/main.js
+++ src/main.js
@@ -4,13 +4,13 @@ import "@/assets/scss/style.scss";
 // import './style.css'
 import App from './App.vue'
 import router from './router';
-import firebase from './assets/config/firebase'
+
 
 const app = createApp(App);
 const pinia = createPinia();
 app.use(router);
 app.use(pinia);
-app.use(firebase)
+
 app.mount('#app')
 
 
