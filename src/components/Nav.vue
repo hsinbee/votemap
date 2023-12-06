2092d27d8772d3d073958436e9bb8c7afbd3dd50
LAPTOP-QCIUTNP8\a0919 <a0919025694@gmail.com>
2023-12-06 18:35:12 +0800

    map 12

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -8,28 +8,28 @@ const navlist = ref([
   { year: 2008, number: 12 },
 ]);
 
+
 const selectNavItem = ref(null);
+
+
+
+
 </script>
 
 <template>
   <div class="nav_container">
     <div class="nav_wrap">
-      <ol>
-        <li
-          v-for="(i, index) in navlist"
-          :key="index"
-          :class="{ selected: selectNavItem === index }"
-        >
+      <ol v-for="(i, index) in navlist" :key="index"
+      >
+      
+        <li :class="{ selected: selectNavItem === index }">
           <router-link
             class="nav_link_top"
             :to="`/${i.year}_vote`"
             @click="selectNavItem = index"
+           
             >{{ i.year }} 年</router-link
-          ><img
-            v-if="$route.path.indexOf(i.year) != -1"
-            src="../assets/images/logo/nav_bear_logo.png"
-            alt="nav_bear"
-          />
+          >
 
           <router-link
             class="nav_link_bottom"
@@ -38,6 +38,12 @@ const selectNavItem = ref(null);
             >第{{ i.number }}任總統選舉</router-link
           >
         </li>
+
+        <img
+          v-if="$route.path.indexOf(i.year) != -1"
+          src="../assets/images/logo/nav_bear_logo.png"
+          alt="nav_bear"
+        />
       </ol>
     </div>
 
@@ -53,53 +59,55 @@ const selectNavItem = ref(null);
   .nav_container {
     // border: 3px solid#b11e1e;
 
-    width: 368px;
+    width: 338px;
     height: 100%;
     display: flex;
     justify-content: center;
     background-color: rgb(255, 255, 255);
     border-radius: 29px;
-    box-shadow: 0px -3px 17px 0px rgba(0, 0, 0, 0.25);
+    box-shadow: 0px -3px 17px 0px rgba(0, 0, 0, 0.25) inset;
+    position: relative;
+    z-index: 999;
+ 
 
     > img {
-      border: 6px solid#b1650f;
-
-      height: 260px;
-      padding-top: 30px;
-      position: relative;
-      z-index: 2;
+      // border: 6px solid#b1650f;
+     padding-top: 20px;
+     width: 18px;
+      height:80%;
+      position: absolute;
+      right: 70px;
+      z-index: -1;
     }
     .nav_wrap {
-      border: 3px solid#140707;
-
-      display: flex;
-
-      align-items: flex-start;
-      position: relative;
-      z-index: 999;
+      // border: 3px solid#140707;
 
       ol {
         // border: 6px solid#0f35b1;
-
         padding: 0;
+        display: flex;
         list-style-type: none;
         text-decoration: none;
         color: map-get($accent_color, accent_color2);
+        
+        > img {
+          // border: 6px solid#0f35b1;
+          height: 50px;
+      
+        }
       }
 
       li {
-        border: 5px solid#0aa521;
+        // border: 5px solid#0aa521;
 
         text-decoration: none;
-
-        margin-bottom: 10px; /* 设置下方间距，可根据需要调整 */
-
-        > img {
-          border: 6px solid#0f35b1;
-        }
+      padding-top: 10px;
+       
+       
       }
       .selected {
         background-color: $web_background;
+        color: black;
       }
     }
   }
