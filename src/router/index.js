7be8bc6d8a6591bf121a25b944020b11b8707d0b
LAPTOP-QCIUTNP8\a0919 <a0919025694@gmail.com>
2023-12-22 00:39:35 +0800

    test

--- src/router/index.js
+++ src/router/index.js
@@ -5,28 +5,28 @@ const routes = [
     {
         path: '/',
         name: 'HomeView',
-        component: () => import('../views/HomeView.vue'),
+        component: () => import('@/views/HomeView.vue'),
     },
 
     {
         path: '/2020_vote',
         name: '2020_vote',
-        component: () => import('../views/2020_vote.vue'),
+        component: () => import('@/views/2020_vote.vue'),
     },
     {
         path: '/2016_vote',
         name: '2016_vote',
-        component: () => import('../views/2016_vote.vue'),
+        component: () => import('@/views/2016_vote.vue'),
     },
     {
         path: '/2012_vote',
         name: '2012_vote',
-        component: () => import('../views/2012_vote.vue'),
+        component: () => import('@/views/2012_vote.vue'),
     },
     {
         path: '/2008_vote',
         name: '2008_vote',
-        component: () => import('../views/2008_vote.vue'),
+        component: () => import('@/views/2008_vote.vue'),
     }
 
 
